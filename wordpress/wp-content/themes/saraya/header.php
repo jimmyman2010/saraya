ba19a2c454212c8b02255859a6784be3da52baa2
MinhMan.Tran <minhman.tran@ogilvy.com>
2016-12-10 11:21:45 +0700

    update wmpl & pagebuilder

--- wordpress/wp-content/themes/saraya/header.php
+++ wordpress/wp-content/themes/saraya/header.php
@@ -13,6 +13,10 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="apple-touch-icon" href="<?= get_template_directory_uri(); ?>/assets/favicon.png">
+	<link rel="shortcut icon" href="<?= get_template_directory_uri(); ?>/assets/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="<?= get_template_directory_uri(); ?>/assets/favicon.ico" type="image/x-icon">
+
 
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
