0855a4bd32c979d1ee721906a2fe12d5bebcff96
MinhMan.Tran <minhman.tran@ogilvy.com>
2017-03-17 17:23:56 +0700

    update wmpl & pagebuilder

--- wordpress/wp-content/themes/saraya/template-parts/content-single.php
+++ wordpress/wp-content/themes/saraya/template-parts/content-single.php
@@ -8,13 +8,24 @@
 ?>
 
 <div class="body">
-
 	<?php the_content(); ?>
-
 </div>
-
 <div class="item">
 	<div class="meta"><?php dickinsons_entry_date(); ?><span class="separate">|</span><?php dickinsons_entry_category(); ?></div>
 	<div class="tags"><?php dickinsons_entry_tag(false); ?></div>
 </div>
 
+<div class="pagination">
+	<div class="nav-links">
+	<?php
+	$prev_post = get_previous_post();
+	if (!empty( $prev_post )): ?>
+		<a class="prev page-numbers" href="<?php echo get_permalink( $prev_post->ID ); ?>"><i class="fa fa-angle-left"></i> PREVIOUS</a>
+	<?php endif ?>
+	<?php
+	$next_post = get_next_post();
+	if (!empty( $next_post )): ?>
+		<a class="next page-numbers" href="<?php echo get_permalink( $next_post->ID ); ?>">NEXT <i class="fa fa-angle-right"></i></a>
+	<?php endif; ?>
+	</div>
+</div>
