514e7c6f76eecefa7f9d4430c4d647bb0ecea7e8
jimmyman2010 <tranduyminhman@gmail.com>
2017-04-18 07:39:46 +0700

    update footer

--- wordpress/wp-content/themes/saraya/inc/my-shortcode.php
+++ wordpress/wp-content/themes/saraya/inc/my-shortcode.php
@@ -14,3 +14,55 @@ function bar_function( $atts ) {
     return '<div class="bar"><span class="color--' . $atts['color'] . '"></span></div>';
 }
 add_shortcode( 'bar', 'bar_function' );
+
+
+function button_func( $atts, $content = "Click here" ) {
+    $atts = shortcode_atts( array(
+        'color' => 'green',
+        'link' => 'javascript:void(0);',
+        'target' => '',
+        'sm' => ''
+    ), $atts, 'button' );
+    if($atts['sm']){
+        $icon = 'fa fa-' . $atts['sm'];
+        if($atts['target']){ } else {
+            $atts['target'] = '_blank';
+        }
+        if($atts['sm'] === 'email'){
+            $icon = 'fa fa-envelope-o';
+            $atts['target'] = '_self';
+        }
+        if($atts['sm'] === 'facebook' || $atts['sm'] === 'twitter'){
+            $icon .= '-square';
+        }
+        $html = '<a target="' . $atts['target'] . '" class="button button--' . $atts['sm'] . '" href="' . $atts['link'] . '"><i class="' . $icon . '"></i>' . $content . '</a>';
+    } else {
+        if($atts['target']){ } else {
+            $atts['target'] = '_self';
+        }
+        $html = '<a target="' . $atts['target'] . '" class="button button--' . $atts['color'] . '" href="' . $atts['link'] . '">' . $content . '</a>';
+    }
+
+    return $html;
+}
+add_shortcode( 'button', 'button_func' );
+
+
+function panel_func( $atts, $content = "Click here" ) {
+    $atts = shortcode_atts( array(
+        'color' => ''
+    ), $atts, 'button' );
+
+    if($atts['color']) {
+        if($atts['color'][0] === '#' && strlen($atts['color']) === 7){
+            $html = '<div class="panel" style="color: ' . $atts['color'] . ';">' . $content . '</div>';
+        } else {
+            $html = '<div class="panel panel--' . $atts['color'] . '">' . $content . '</div>';
+        }
+    } else {
+        $html = '<div class="panel">' . $content . '</div>';
+    }
+
+    return $html;
+}
+add_shortcode( 'panel', 'panel_func' );
