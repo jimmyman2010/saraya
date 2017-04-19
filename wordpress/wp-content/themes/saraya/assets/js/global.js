0635360894cdfacebf5c436ce2529cf12ba5fe7d
MinhMan.Tran <minhman.tran@ogilvy.com>
2017-04-19 18:53:39 +0700

    update wmpl & pagebuilder

--- wordpress/wp-content/themes/saraya/assets/js/global.js
+++ wordpress/wp-content/themes/saraya/assets/js/global.js
@@ -177,5 +177,26 @@ $(function(){
         });
     }
 
+    var popupGallery = $('.popup-gallery');
+    if(popupGallery.length > 0) {
+        popupGallery.magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'/*,
+                titleSrc: function (item) {
+                    return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+                }*/
+            }
+        });
+    }
+
     DKS.init();
 });
